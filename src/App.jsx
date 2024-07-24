c1a56c57ccc5f748e951b7b44b1d6bca019411bf
AlexFromNorth_desc <jacklex.ab@gmail.com>
2024-07-24 18:36:33 +0300

    created todoList with store

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+
+import { addTodo } from './store/todoSlice';
+import NewTodoForm from './components/NewTodoForm';
+import TodoList from './components/TodoList';
+
+import './App.css';
+import { useDispatch } from 'react-redux';
+
+
+function App() {
+  const [text, setText] = useState('');
+  const dispatch = useDispatch();
+
+  const handleAction = () => {
+    if(text.trim().length) {
+      dispatch(addTodo({text}));
+      setText('');
+    }
+  }
+
+  return (
+    <div className='App'>
+      <NewTodoForm
+        value={text}
+        updateText={setText}
+        handleAction={handleAction}
+      />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
